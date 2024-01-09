9dd7f741dc890a66c22999cf82296b0b0bdb22ff
HuyDuong <93182239+FeikVizeege@users.noreply.github.com>
2024-01-09 10:35:53 +0700

    Update + Delete Event

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,10 +1,10 @@
-import { getEventsById } from "@/lib/actions/event.actions";
+import { getEventById } from "@/lib/actions/event.actions";
 import { formatDateTime } from "@/lib/utils";
 import { SearchParamProps } from "@/types";
 import Image from "next/image";
 
 const EventDetails = async ({ params: { id } }: SearchParamProps) => {
-    const event = await getEventsById(id);
+    const event = await getEventById(id);
     return ( 
         <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
             <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
